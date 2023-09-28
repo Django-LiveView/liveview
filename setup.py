c6f604dba643391aa799e66ee523559ec8e5ef42
Andros Fenollosa <andros@fenollosa.email>
2023-09-28 10:48:28 +0200

    Merge

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 setup(
     name="django-liveview",
     py_modules=["liveview"],
-    version="VERSION",
+    version="1.0.2",
     python_requires=">3.7",
     description="Framework for creating Realtime SPAs using HTML over the Wire technology in Django",
     author="Andros Fenollosa",
     author_email="andros@fenollosa.email",
-    url="https://github.com/Django-LiveView/liveview",
+    url="https://django-liveview.andros.dev/",
+    license="MIT License",
+    platforms=['any'],
+    packages=["liveview"],
     keywords=["django", "ssr", "channels", "liveview", "html-over-the-wire", "hotwire"],
     classifiers=[
         "Programming Language :: Python :: 3",
